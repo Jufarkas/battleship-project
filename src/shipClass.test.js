0082f44946e657e2699849b7ed5fb83290571f12
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2024-04-26 13:08:29 -0700

    test: add tests to confirm ship properties are updating as expected

---
+++ src/shipClass.test.js
@@ -0,0 +1,33 @@
+import { experiments } from "webpack";
+import { Ship } from "./shipClass";
+
+const newShip = new Ship('carrier', 5);
+const anotherShip = new Ship('lifeRaft', 1);
+
+test('ship is created, length 5', () => {
+    expect(newShip).toEqual({name:'carrier', length: 5, hits: 0, sunk: false});
+});
+
+test('another ship is created, length 1', () => {
+    expect(anotherShip).toEqual({name: 'lifeRaft', length: 1, hits: 0, sunk: false})
+})
+
+test('ship is hit', () => {
+    expect(newShip.hit()).toEqual('hit');
+    expect(newShip.hits).toEqual(1);
+    console.log(newShip);
+});
+
+test('ship is hit again', () => {
+    expect(newShip.hit()).toEqual('hit');
+    expect(newShip.hits).toEqual(2);
+    expect(newShip.hit()).toEqual('hit');
+    expect(newShip.hits).toEqual(3);
+    console.log(newShip);
+});
+
+test('anotherShip is sunk', () => {
+    expect(anotherShip.hit()).toEqual('sunk');
+    expect(anotherShip.sunk).toEqual(true);
+    console.log(anotherShip);
+});
